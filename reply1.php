f6c93b22cd89224bd012b624d8d59f5a28196547
Asit-website <84184502+Asit-website@users.noreply.github.com>
2021-07-22 23:20:25 +0800

    Update reply1.php

--- reply1.php
+++ reply1.php
@@ -208,7 +208,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    
+      <script>
+    function toggleHide(e) {
+        let bsn = document.getElementById('bsn');
+        let laravel = document.getElementById('laravel');
 
+        if (laravel.style.display != 'none') {
+            laravel.style.display = 'none';
+            e.preventDefault();
+        } else {
+            laravel.style.display = 'block';
+        }
+    }
+   
+    </script>
   
 
 
